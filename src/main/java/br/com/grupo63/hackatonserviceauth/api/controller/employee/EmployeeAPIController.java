7676329e78da24cd6ca2b321531efd6917e3e43f
Thiago Bezerra <thiago.bezsilva@gmail.com>
2024-03-22 20:15:31 -0300

    fix: use private key to encrypt

--- src/main/java/br/com/grupo63/hackatonserviceauth/api/controller/employee/EmployeeAPIController.java
+++ src/main/java/br/com/grupo63/hackatonserviceauth/api/controller/employee/EmployeeAPIController.java
@@ -11,6 +11,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
 @Tag(name = "empregados", description = "CRUD de empregados para gerenciamento")
 @RequiredArgsConstructor
 @RestController
@@ -30,9 +34,12 @@ public class EmployeeAPIController extends AbstractAPIController {
             EmployeeControllerDTO employeeDTO = new EmployeeControllerDTO(registrationNumber, password, null);
             EmployeeControllerDTO existingEmployee = employeeController.identify(employeeDTO);
             String token = jwtService.createToken(Long.toString(existingEmployee.getId()), existingEmployee.getEmail());
+
             return ResponseEntity.ok(token);
         } catch (NotFoundException exception) {
             return ResponseEntity.notFound().build();
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            return ResponseEntity.internalServerError().build();
         }
     }
 
